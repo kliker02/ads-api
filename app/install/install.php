ea617e356ca2bb4242404679d7f628ecb3a7906c
Aleksandr <laker.tv@bk.ru>
2021-09-23 22:36:50 +0300

    execute install

--- app/install/install.php
+++ app/install/install.php
@@ -7,4 +7,4 @@ $mysql_password = "";
 # MySQL with PDO_MYSQL
 $db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
 $query = file_get_contents("/app/app/install/ads_project.sql");
-$stmt = $db->prepare($query);
+$stmt = $db->exec($query);
